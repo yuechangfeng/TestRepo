e9a97f2949900f3db204a0774755bdf3b563f931
长风 <1640896090@qq.com>
2019-03-07 15:44:19 +0800

    Initial Commit

---
+++ TestOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestOne
+//
+//  Created by yuecf on 2019/3/7.
+//  Copyright © 2019年 Bsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestOne
+//
+//  Created by yuecf on 2019/3/7.
+//  Copyright © 2019年 Bsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
